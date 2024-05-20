9264adcaea1883f892375560b14daba0c5bae5e3
Gabriel Ferraz <151977038+GabrielFerrazzzzz@users.noreply.github.com>
2024-05-20 16:27:08 -0300

    Novas Atualizações
    Novas Atualizações

--- Projeto-Tech-Sidel-part-2--main/index.js
+++ Projeto-Tech-Sidel-part-2--main/index.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 
     function animationData() {
         let duration = [1400, 1600, 2000, 2200];
-        let number = [317, 265, 42,  10];
+        let number = [2110, 2010, 90,  10];
         let colors = ['#00a2ff', '#14e737', '#ff7f00', '#ff0000']; // Changed the last color to red
         let c = 65;
         for (let toCircle = 0; toCircle < 4; toCircle++) {
@@ -35,4 +35,48 @@ $(document).ready(function () {
     $("#mainSlider").parallax({
         imageSrc: 'img-carousel/pattern.png'
     });
+
 });
+
+$(document).ready(function () {
+    // progress bar
+    let dataAreaOffset = $("data-area-hourly").offset();
+
+    function animationData() {
+        let duration = [1400, 1600, 2000, 2200];
+        let number = [317, 265, 42,  10];
+        let colors = ['#00a2ff', '#14e737', '#ff7f00', '#ff0000']; // Changed the last color to red
+        let c = 65;
+        for (let toCircle = 0; toCircle < 4; toCircle++) {
+            let container = document.getElementById("circulo" + String.fromCharCode(c + toCircle));
+            let circle = new ProgressBar.Circle(container, {
+                color: colors[toCircle],
+                strokeWidth: 5,
+                duration: duration[toCircle],
+                from: { color: '#AAA' },
+                to: { color: colors[toCircle] },
+                step: function (state, circle) {
+                    circle.path.setAttribute('stroke', state.color);
+                    let value = Math.round(circle.value() * number[toCircle]);
+                    circle.setText(value);
+                }
+            });
+
+            circle.animate(1.0); // Ensure the animation starts
+        }
+    }
+
+    animationData(); // Chama a função de animação assim que a página é carregada
+
+    $("data-area-hourly").parallax({
+        imageSrc: 'img-carousel/cidadeparallax.png'
+    });
+
+    $("#mainSlider").parallax({
+        imageSrc: 'img-carousel/pattern.png'
+    });
+
+});
+
+
+
