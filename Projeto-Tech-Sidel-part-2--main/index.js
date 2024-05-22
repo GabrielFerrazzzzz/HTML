f0d56424174ab7e68abfb2fcbcbb0529a35becc2
Gabriel Ferraz <151977038+GabrielFerrazzzzz@users.noreply.github.com>
2024-05-21 22:57:01 -0300

    Final
    Parte Final

--- Projeto-Tech-Sidel-part-2--main/index.js
+++ Projeto-Tech-Sidel-part-2--main/index.js
@@ -47,28 +47,30 @@ $(document).ready(function () {
         let number = [317, 265, 42,  10];
         let colors = ['#00a2ff', '#14e737', '#ff7f00', '#ff0000']; // Changed the last color to red
         let c = 65;
-        for (let toCircle = 0; toCircle < 4; toCircle++) {
-            let container = document.getElementById("circulo" + String.fromCharCode(c + toCircle));
-            let circle = new ProgressBar.Circle(container, {
-                color: colors[toCircle],
+        for (let toCirculo = 0; toCirculo < 4; toCirculo++) {
+            let container = document.getElementById("circulo" + String.fromCharCode(c + toCirculo));
+            let circulo = new ProgressBar.Circle(container, {
+                color: colors[toCirculo],
                 strokeWidth: 5,
-                duration: duration[toCircle],
+                duration: duration[toCirculo],
                 from: { color: '#AAA' },
-                to: { color: colors[toCircle] },
-                step: function (state, circle) {
-                    circle.path.setAttribute('stroke', state.color);
-                    let value = Math.round(circle.value() * number[toCircle]);
-                    circle.setText(value);
+                to: { color: colors[toCirculo] },
+                step: function (state, circulo) {
+                    circulo.path.setAttribute('stroke', state.color);
+                    let value = Math.round(circulo.value() * number[toCirculo]);
+                    circulo.setText(value);
                 }
             });
 
-            circle.animate(1.0); // Ensure the animation starts
+            circulo.animate(1.0); // Ensure the animation starts
         }
     }
 
+    
+
     animationData(); // Chama a função de animação assim que a página é carregada
 
-    $("data-area-hourly").parallax({
+    $("#data-area-hourly").parallax({
         imageSrc: 'img-carousel/cidadeparallax.png'
     });
 
